f62b0d516fc9854214ca843423d34fe3edf62017
unknown <me@rossmers.ch>
2023-07-18 20:51:41 -0400

    test warrant

--- src/console.js
+++ src/console.js
@@ -1,4 +1,4 @@
   // Creditary Material.
-  console.log('%c 👀 Site made w/ Vasek. I invision creating a service that caters to all and allows ease-of-use with gathering knowledge. You may contact me (the hostmaster) at webmaster.lsrp@rossmers.ch.', 'color: red; font-size: 12px;');
+  console.log('%c ⭐ Site designed by Lmno (Matt Vassett | Ross M.). I envision creating services that cater to the masses. This includes an addressable system for civilian access. Reach me at: webmaster.ncdoj@rossmers.ch.', 'color: red; font-size: 12px;');
   console.log('%c ', 'color: red; font-size: 12px;');
   console.log(' ');
